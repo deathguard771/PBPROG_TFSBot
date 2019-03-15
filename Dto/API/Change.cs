3f4518d53e8167b0f31aae44cfe06782a841440b
Alexandre Kislitsyn <4lex.kislitsyn@gmail.com>
2019-03-16 00:51:41 +0300

    Changed enum to string

--- Dto/API/Change.cs
+++ Dto/API/Change.cs
@@ -4,11 +4,9 @@ namespace BasicBot.Dto.API
 {
     public class Change
     {
-        public ChangeTypes ChangeType { get; set; }
+        public string ChangeType { get; set; }
 
         [JsonProperty("item")]
         public ChangeInfo Info { get; set; }
-
-        public enum ChangeTypes { Add, Edit, Remove}
     }
 }
